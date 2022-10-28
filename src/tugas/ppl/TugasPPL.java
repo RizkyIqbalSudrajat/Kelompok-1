2e76b0d49d681b3aa72bbd8341d2ffb369831457
RizkyIqbalSudrajat <rizkyiqbal2005@gmail.com>
2022-10-28 14:24:28 +0700

    first commit

---
+++ src/tugas/ppl/TugasPPL.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas.ppl;
+
+/**
+ *
+ * @author Rizky
+ */
+public class TugasPPL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama= "kelompok 1";
+      int nisn= 542211034;
+      String kelas="XI RPL 3";
+      String alamat_mlg="Jl. Danau Poso 1 blok G2E no. 11 RT 01 RW 05 Sawojajar Malang 65139";
+      int no_telpon= 999999999;
+      
+      String motto_hidup="hidup seperti lerry";
+      
+      System.out.println("NISN             :"+nisn);
+      System.out.println("nama            :"+nama);
+      System.out.println("kelas           :"+kelas);
+      System.out.println("alamat          :"+alamat_mlg);
+      System.out.println("no_telpon      :"+no_telpon);
+
+    }
+    
+}
